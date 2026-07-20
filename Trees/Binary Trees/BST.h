1be4b867896f9e9e44f96b03aa2a9a06a6a603e3
chisaiki <mishitarahman@gmail.com>
2026-07-20 01:20:15 -0400

    working on BST

--- Trees/Binary Trees/BST.h
+++ Trees/Binary Trees/BST.h
@@ -1,21 +1,28 @@
 #ifndef BST_H_
 #define BST_H_ 
 
-template<T> 
+template<typename T> 
 struct TreeNode{
     T value; 
-    TreeNode* leftchild; 
-    TreeNode* rightchild; 
+    TreeNode<T>* leftchild; 
+    TreeNode<T>* rightchild; 
 
-    TreeNode(T val = {}, TreeNode* initialleft = nullptr, TreeNode* initialright = nullptr) : value(val), leftchild(initialleft), rightchild(initialright);
+    TreeNode(T val = {}, TreeNode<T>* initialleft = nullptr, TreeNode<T>* initialright = nullptr) : value(val), leftchild(initialleft), rightchild(initialright){};
 };
 
+template<typename T>  
 class BST{
     private:
-        TreeNode* root;
+        TreeNode<T>* root_ = nullptr;
 
     public:
-        
+        T GetValue() const{
+            return this->root_->value;
+        };
+
+        BST(TreeNode<T>* root = new TreeNode<T>){
+            root_ = root;
+        }
     
 };
 
--- Trees/Binary Trees/main.cpp
+++ Trees/Binary Trees/main.cpp
@@ -1,6 +1,12 @@
 #include "BST.h"
+#include <iostream>
 
 int main(){
-    
+    TreeNode<int>* obj = new TreeNode<int>(2);
+
+    BST<int> NewObject;
+
+    std::cout << NewObject.GetValue() << std::endl;
+
     return 0;
 }
